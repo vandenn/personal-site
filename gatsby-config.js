481f57ca815ee3205b1799ea007c478424702aad
Vandenn <evan.livelo@gmail.com>
2020-09-02 15:40:27 +0800

    build: Add several gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,22 @@ const path = require('path');
 
 module.exports = {
   plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
         src: path.join(__dirname, 'src'),
         components: path.join(__dirname, 'src/components'),
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content/`,
       },
     },
   ],
