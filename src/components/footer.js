2b3adc7a76796e2021180ce59c266dfc30782be3
Vandenn <evan.livelo@gmail.com>
2020-09-03 18:51:54 +0800

    style: Stylize footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Link, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import constants from 'constants/common';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '20px',
+    textAlign: 'center',
+  },
+}));
+
 const Footer = (props) => {
+  const classes = useStyles();
+
   return (
-    <center>
-      <a href={`${constants.github}`}>Built by {constants.name}</a>
-    </center>
+    <Paper elevation={0} className={classes.root}>
+      <Typography component={Link} href={`${constants.github}`}>
+        Built by {constants.name}
+      </Typography>
+    </Paper>
   );
 };
 
