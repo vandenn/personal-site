6c45d83dd3a33b9ce13b53d0d5e996a0447187bf
Vandenn <evan.livelo@gmail.com>
2020-09-03 23:12:29 +0800

    style: Widen sections

--- src/components/sections/sectionLayout.js
+++ src/components/sections/sectionLayout.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: '50px 100px 50px 100px',
+    margin: '50px',
     padding: '50px',
   },
 }));
