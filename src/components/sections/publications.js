4692c9071c669189a707c77e796fa2058fdce2d2
Vandenn <evan.livelo@gmail.com>
2020-09-03 23:39:03 +0800

    style: Paperize publications section

--- src/components/sections/publications.js
+++ src/components/sections/publications.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Grid, Link, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import SectionLayout from './sectionLayout';
 import navigationConstants from 'constants/navigation';
 
+const useStyles = makeStyles((theme) => ({
+  content: {
+    marginTop: theme.spacing(2),
+  },
+  publicationEntry: {
+    padding: theme.spacing(2),
+  },
+}));
+
 const Publications = (props) => {
+  const classes = useStyles();
   const { data } = props;
 
   const renderPublicationEntry = (node, index) => {
     const { html, frontmatter } = node;
     const { title, coauthors, publisher, link } = frontmatter;
+    const allocatedSize = Math.max(4, 12 / data.length);
     return (
-      <div key={index}>
-        <h2>{title}</h2>
-        <h3>{publisher}</h3>
-        <h4>{`With: ${coauthors}`}</h4>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <a href={link}>View</a>
-      </div>
+      <Grid item key={index} xs={12} sm={allocatedSize}>
+        <Paper variant='outlined' square className={classes.publicationEntry}>
+          <Typography variant='h6'>{title}</Typography>
+          <Typography variant='subtitle1' color='textSecondary'>
+            {publisher}
+          </Typography>
+          <Typography variant='subtitle1'>{`With: ${coauthors}`}</Typography>
+          <Typography dangerouslySetInnerHTML={{ __html: html }} />
+          <Link href={link} target='_blank' rel='noopener'>
+            View
+          </Link>
+        </Paper>
+      </Grid>
     );
   };
 
@@ -27,7 +45,9 @@ const Publications = (props) => {
       <Typography variant='h3'>
         Publications I've made with wonderful people
       </Typography>
-      {data.map(({ node }, index) => renderPublicationEntry(node, index))}
+      <Grid container spacing={2} className={classes.content}>
+        {data.map(({ node }, index) => renderPublicationEntry(node, index))}
+      </Grid>
     </SectionLayout>
   );
 };
