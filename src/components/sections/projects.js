773313890ea6ad849dd1a6b5e6edb17171333f18
Vandenn <evan.livelo@gmail.com>
2020-09-03 23:44:57 +0800

    style: Paperize projects section

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Grid, Link, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import SectionLayout from './sectionLayout';
 import navigationConstants from 'constants/navigation';
 
+const useStyles = makeStyles((theme) => ({
+  content: {
+    marginTop: theme.spacing(2),
+  },
+  projectEntry: {
+    padding: theme.spacing(2),
+  },
+}));
+
 const Projects = (props) => {
+  const classes = useStyles();
   const { data } = props;
 
   const renderProjectEntry = (node, projectIndex) => {
     const { html, frontmatter } = node;
     const { title, tech, link } = frontmatter;
+    const allocatedSize = Math.max(4, 12 / data.length);
 
     const renderTech = () => {
       return (
         <>
-          <h4>Tech</h4>
+          <Typography variant='subtitle1'>Tech</Typography>
           <ul>
             {tech.map((techEntry, techIndex) => {
               return <li key={techIndex}>{techEntry}</li>;
@@ -26,19 +38,29 @@ const Projects = (props) => {
     };
 
     return (
-      <div key={projectIndex}>
-        <h2>{title}</h2>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        {renderTech()}
-        <a href={link}>View</a>
-      </div>
+      <Grid item key={projectIndex} xs={12} sm={allocatedSize}>
+        <Paper
+          elevation={0}
+          variant='outlined'
+          className={classes.projectEntry}
+        >
+          <Typography variant='h6'>{title}</Typography>
+          <Typography dangerouslySetInnerHTML={{ __html: html }} />
+          {renderTech()}
+          <Link href={link} target='_blank' rel='noopener'>
+            View
+          </Link>
+        </Paper>
+      </Grid>
     );
   };
 
   return (
     <SectionLayout id={navigationConstants.projects.id}>
-      <Typography variant='h3'>Some interesting projects I've made</Typography>
-      {data.map(({ node }, index) => renderProjectEntry(node, index))}
+      <Typography variant='h3'>Some interesting projects I've done</Typography>
+      <Grid container spacing={2} className={classes.content}>
+        {data.map(({ node }, index) => renderProjectEntry(node, index))}
+      </Grid>
     </SectionLayout>
   );
 };
